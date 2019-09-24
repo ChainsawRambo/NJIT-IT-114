66445b8d297ba111cb9ef1bf308dc7a008a8cc96
ChainsawRambo <54954995+ChainsawRambo@users.noreply.github.com>
2019-09-24 18:06:28 -0400

    Add files via upload

--- SampleSocketServer.java
+++ SampleSocketServer.java
@@ -6,7 +6,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 public class SampleSocketServer {
+int port = 3001;
+public SampleSocketServer() {
+}
 public void start(int port) {
+this.port = port;
 System.out.println("Waiting for client connection...");
 try(//steps to create socket, accept connection and setup input/output "channels"
 ServerSocket serverSocket = new ServerSocket(port);
@@ -17,16 +21,26 @@ BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInp
 System.out.println("Client connected, waiting for message");
 String fromClient = "", toClient="";
 while((fromClient = in.readLine()) != null) {
-System.out.println("Message from client: " + fromClient);
+//System.out.println("Message from client: " + fromClient);
 if("kill server".equalsIgnoreCase(fromClient)) {
 System.out.println("Client killed server process");
 break;
+}
+else {
+System.out.println("From client: " + fromClient);
 }
 }
 
 }
 catch(Exception e) {
 e.printStackTrace();
+}
+finally {
+try {
+System.out.println("closing server socket");
+} catch (Exception e) {
+e.printStackTrace();
+}
 }
 }
 public static void main(String[] args) {
